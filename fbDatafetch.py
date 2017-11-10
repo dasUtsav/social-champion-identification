84408b12f24d7bf79f138c6791ce9e3593b1d9b2
PSIB0T <arvindonarayanan@gmail.com>
2017-11-10 21:06:35 +0530

    Fetch page likes, post likes and message of each post

--- fbDatafetch.py
+++ fbDatafetch.py
@@ -1,5 +1,7 @@
 import facebook
 
+from classes.Post import Post
+
 access_token = "EAABZBfg9w4HgBAC58ta4cbc0ZBZA5HNmF6RPmu4IOjdnlnXz376iWUZCnIhXK9k5qmYOwjH5EeaTACmnRvGEGt97eKOVlZAkcFAIk4NIy729sagoVpt1Gr8qpkUXplEyj7ROt1wWTHrdNvRRUNg2Cyr5poFwZB7QjZAK1L8I2ACqefrLw4mHTbzZBJVHzCG9ZAqZBviQL8AjVanQZDZD"
 
 # Initialize graph object
@@ -17,12 +19,22 @@ usernames = ['RUR.AreYouReducingReusingRecycling', 'EARTHOHOLICS']
 pageDict = []
 
 for username in usernames:
-    pageDict.append({'name': username})
+    pageDict.append({'name': username, 'data': []})
 pageDict = sorted(pageDict, key=lambda page: page['name'].lower())
 
 
-getPageLikes = graph.get_objects(ids=usernames, fields='fan_count')
+getPageLikes = graph.get_objects(ids=usernames, fields='feed{likes.summary(true), message}, fan_count')
 
 for username in getPageLikes:
     item = findInListDict(pageDict, 'name', username)
-    item['likes'] = getPageLikes[username]['fan_count']
+    item['page_likes'] = getPageLikes[username]['fan_count']
+    for data in getPageLikes[username]['feed']['data']:
+        message = ""
+        if('message' in data):
+            message = data['message']
+        item['data'].append(Post(data['id'], message, data['likes']['summary']['total_count']))
+                            
+for post in pageDict[1]['data']:
+    print(post.postId)
+        
+

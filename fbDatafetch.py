e8fbf04ef4673052918594c23a595a32599608f3
Utsav Das <2015utsav.das@ves.ac.in>
2017-11-12 20:07:57 +0530

    Refined basic ranking formula to give a normalized percentile score based on page likes and average likes per post

--- fbDatafetch.py
+++ fbDatafetch.py
@@ -34,7 +34,7 @@ class DataFetch_fb:
         return pageDict
 
 
-access_token = "EAACEdEose0cBANWz94zTj1EpkYX60BaXfT9unkiUZCEkEVXlTRjNrw1m6ce7yqv2R6G7vjeWKc0A1ZBKocMMi8WqMkf2J4XNQhd0Jwuklsrih2VtlE9iTn9VW7K2or3HZA6C1wb9xftQaoznnkdN7qEsCPFKZCA2hbMfrTMZC7risaKbc4LIHs8gVqZBIXLRZCUvYdYUOHmKgZDZD"
+access_token = "EAACEdEose0cBAPZCEA4jZBgxMnoqoMzEqT8714tC2VQVBwF9l4TLfJCnFSHwwjESVwoEhYqppaSYZCJHs2VwQyZAC8OtE23f7dKlLQZCpK0hpV27uRjRytCwF7oZCge4qVZBX9YEM6YattTzLeAGNrllil8iZAKmxCo32vZBqIVwvLZC3GRCUF4YrI9Ef5PFT785oZD"
 
 # Profile usernames
 usernames = ['RUR.AreYouReducingReusingRecycling', 'EARTHOHOLICS']
@@ -46,15 +46,44 @@ result = fetchDb.fetchPosts()
 totalLikes = count = avg = 0
 likes = []
 
+rankPageLikes = []
+rankPostLikes = []
+
 for page in result:
     #totalLikes = page['page_likes']
     for data in page['data']:
-        count += 1
         likes.append(data.likes)
         totalLikes = sum(likes)
-    avg = totalLikes/count
-    score = (((avg - 0) * 10) / 100) + 0
-    print("Username: ", page['name'], "Score: ", int(score))
+    avg = totalLikes/len(page['data'])
+    rankPageLikes.append({'name': page['name'], 'likes': page['page_likes']})
+    rankPostLikes.append({'name': page['name'], 'likes': avg})
+    # print("Username: ", page['name'], "Score: ", int(score))
+
+rankPageLikes = sorted(rankPageLikes, key=lambda page : page['likes'], reverse=True)
+rankPostLikes = sorted(rankPostLikes, key=lambda page : page['likes'], reverse=True)
+#print(rankPostLikes)
+
+rankList = []
+arrayLen = len(rankPageLikes)
+
+for i in range(0, arrayLen):
+    rankPageLikes[i]['rank'] = rankPostLikes[i]['rank'] = arrayLen - i - 1
+
+def getFromDict(list, key, value):
+    for item in list:
+        if(item[key] == value):
+            return item
+    return {}
+
+for username in usernames:
+    page = getFromDict(rankPageLikes, 'name', username)
+    post = getFromDict(rankPostLikes, 'name', username)
+    finalScore = (0.6 * post['rank'] + 0.4 * page['rank'])/arrayLen * 100
+    rankList.append({"name":username, 'finalScore': finalScore})
+
+print(rankList)
+
+
 
 # for page in result:
 #     print(len(page['data']))

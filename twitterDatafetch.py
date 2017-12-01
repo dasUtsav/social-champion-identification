5585027abce9a3df4bd79c3f79f0ba1b031ad1d0
PSIB0T <arvindonarayanan@gmail.com>
2017-12-01 12:40:29 +0530

    Add: output format for twitter

--- twitterDatafetch.py
+++ twitterDatafetch.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 consumer_key = "rLSO59KOXRjjWisGpsuqqaEdJ"
 consumer_secret = "rprRhqIsgxY49qFZlpRZmZs7pMXcFoJrGmryoRUUmXRQm6E9v0"
@@ -11,11 +12,33 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+op_file = open("output.json", "w")
+
 # public_tweets = api.home_timeline()
 # for tweet in public_tweets:
 #     print(tweet.text)
 
 user = api.get_user('realDonaldTrump')
+
+def todict(obj, classkey=None):
+    if isinstance(obj, dict):
+        data = {}
+        for (k, v) in obj.items():
+            data[k] = todict(v, classkey)
+        return data
+    elif hasattr(obj, "_ast"):
+        return todict(obj._ast())
+    elif hasattr(obj, "__iter__"):
+        return [todict(v, classkey) for v in obj]
+    elif hasattr(obj, "__dict__"):
+        data = dict([(key, todict(value, classkey)) 
+            for key, value in obj.__dict__.items() 
+            if not callable(value) and not key.startswith('_')])
+        if classkey is not None and hasattr(obj, "__class__"):
+            data[classkey] = obj.__class__.__name__
+        return data
+    else:
+        return obj
 # print(user.screen_name)
 # print(user.followers_count)
 # for friend in user.friends():
@@ -23,6 +46,10 @@ user = api.get_user('realDonaldTrump')
 # followers = api.followers(user.screen_name)
 # print(len(followers))
 number=0
-for status in tweepy.Cursor(api.user_timeline, id=user.screen_name).items(5):
-    print(status.text)
+res = {}
+for status in tweepy.Cursor(api.user_timeline, id=user.screen_name, tweet_mode='extended').items(1):
+    res = json.dumps(status._json)
+    op_file.write(res)
+op_file.close()
+
     

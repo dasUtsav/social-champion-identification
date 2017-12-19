49f11a7971223587ea0b73bfea66d0ffee2b8dfe
Utsav Das <2015utsav.das@ves.ac.in>
2017-12-19 17:41:41 +0530

    Mongodb integration

--- twitterDatafetch.py
+++ twitterDatafetch.py
@@ -1,5 +1,6 @@
 import tweepy
 import json
+from pymongo import MongoClient
 
 consumer_key = "rLSO59KOXRjjWisGpsuqqaEdJ"
 consumer_secret = "rprRhqIsgxY49qFZlpRZmZs7pMXcFoJrGmryoRUUmXRQm6E9v0"
@@ -12,7 +13,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-op_file = open("output.json", "w")
+# op_file = open("output.json", "w")
 
 # public_tweets = api.home_timeline()
 # for tweet in public_tweets:
@@ -26,11 +27,25 @@ user = api.get_user('realDonaldTrump')
 #    print(friend.screen_name)
 # followers = api.followers(user.screen_name)
 # print(len(followers))
-number=0
-res = {}
-for status in tweepy.Cursor(api.user_timeline, id=user.screen_name, tweet_mode='extended').items(1):
-    res = json.dumps(status._json)
-    op_file.write(res)
-op_file.close()
+# res = {}
+# for status in tweepy.Cursor(api.user_timeline, id=user.screen_name, tweet_mode='extended').items(6):
+#     res = json.dumps(status._json)
+#     op_file.write(res)
+# op_file.close()
 
-    
+client = MongoClient('mongodb://localhost:27017/')
+db = client['twitter-db']
+collection = db['twitter-collection']
+collection.remove({})
+
+for status in tweepy.Cursor(api.user_timeline, id=user.screen_name, tweet_mode='extended').items():
+    # status = json.loads(status._json)
+    # print(status.full_text)
+    collection.insert(status._json)
+
+# tweets = collection.find()
+# for tweet in tweets:
+#     print(tweet['full_text'])
+#     print(tweet['retweet_count'])
+
+print(collection.count())

cb08b92dbd06dd463679874995a0fb06dd1074f0
Utsav Das <2015utsav.das@ves.ac.in>
2018-01-03 16:10:15 +0530

    Demo 1 notebook and files

---
+++ demo-1/credentials.py
@@ -0,0 +1,5 @@
+consumer_key = "rLSO59KOXRjjWisGpsuqqaEdJ"
+consumer_secret = "rprRhqIsgxY49qFZlpRZmZs7pMXcFoJrGmryoRUUmXRQm6E9v0"
+
+access_token = "420400261-Qknhgi9XaPv1ZklKdV68Ef4ZzbmjyPTFKynzW2WR"
+access_token_secret = "B077y0aYdHAntBR0GKB84xHqkofTW2a99qb42iptprDwE"
